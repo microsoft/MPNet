df315c78f1ca28926ba0789eaacab176a8449002
KaiTao Song <sktsxy@gmail.com>
2020-06-11 22:34:40 +0800

    Update permutation_utils.pyx

--- pretraining/fairseq/data/permutation_utils.pyx
+++ pretraining/fairseq/data/permutation_utils.pyx
@@ -1,8 +1,8 @@
-i# Copyright (c) Microsoft Corporation. All rights reserved.
+# Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 #
 
-mport numpy as np
+import numpy as np
 
 cimport cython
 cimport numpy as np
